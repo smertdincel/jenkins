84356463c379c59d72f546bf6f13e17c287cea70
Sadık Mert Dincel <sadikmertdincel@gmail.com>
2025-08-19 12:15:05 +0300

    feat: update home page message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Merhaba! CI/CD + Kubernetes (Minikube) üzerinde Flask Sadık mert taradıdan yapıldı 🎉"
+    return "Merhaba! CI/CD + Kubernetes (Minikube) üzerinde Flask Sadik taradıdan yapıldı  🎉"
 
 @app.route("/health")
 def health():
